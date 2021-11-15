12b02d734220a1b9eebb7579248526ca53c8f174
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-15 23:03:20 +0530

    Remove span xss

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,21 +2,62 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('Programming');
+    const [results, setResults] = useState([]);
+    console.log(results);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('');
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+            setResults(data.query.search);
         }
         search();
 
         // (async () => {
-        //     await axios.get('');
+        //     await axios.get('https://en.wikipedia.org/w/api.php', {
+        //         params: {
+        //             action: 'query',
+        //             list: 'search',
+        //             origin: '*',
+        //             format: 'json',
+        //             srsearch: term
+        //         }
+        //     });
         // })();
 
-        //await.get('').then((response)=>{console.log(response.data)});
+        // await.get('https://en.wikipedia.org/w/api.php', {
+        //     params: {
+        //         action: 'query',
+        //         list: 'search',
+        //         origin: '*',
+        //         format: 'json',
+        //         srsearch: term
+        //     }
+        // }).then((response) => { console.log(response.data) });
 
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+                    {/* {result.snippet} */}
+                </div>
+            </div>
+        )
+    })
 
     const onChangeInput = (e) => {
         setTerm(e.target.value);
@@ -29,6 +70,9 @@ const Search = () => {
                     <label htmlFor="serchTermInput">Enter Search Term</label>
                     <input type="text" name="" value={term} id="serchTermInput" className="input" onChange={onChangeInput} />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
