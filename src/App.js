bc650e01acc5a08af0ad51e99467193da34d3113
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-19 23:02:49 +0530

    Creating Dropdown Js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
 import Search from './components/Search'
 
 const items = [
@@ -15,12 +16,27 @@ const items = [
         title: "Mandya",
         content: "Our District"
     }
-]
+];
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Shade of Blue',
+        value: 'blue'
+    }
+];
 
 export default () => {
     return (
-        <div className="ui container">
-            <Search />
+        <div className="">
+            <Dropdown options={options} />
         </div>
     )
 }
