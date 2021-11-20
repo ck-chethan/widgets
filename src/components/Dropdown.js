45e4d562a4305449f1ee09020d543485517f2cad
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-20 14:21:38 +0530

    Making use of useRef to close dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();//Using this we're getting the reference of most level element in the Dropdown
 
     useEffect(() => {
-        document.body.addEventListener('click', () => {
-            console.log("BODY CLICKED");
+        document.body.addEventListener('click', (event) => {
+            //console.log(event.target);
+            if (ref.current.contains(event.target)) {
+                return;
+            }
             setOpen(false);
         }, { capture: true });
     }, []);
@@ -16,22 +20,16 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         }
         return (<div key={option.value}
             onClick={() => {
-                console.log("ITEM CLICKED");
                 onSelectedChange(option)
             }}
             className="item">{option.label}</div>);
     });
-    //Add event Listeners will call first then React listeners will call from Child and bobbling up
+    //console.log(ref.current);
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label htmlFor="" className="label">Select a Color</label>
-                <div onClick={() => {
-                    console.log("DROPDOWN CLICKED");
-                    setOpen(!open)
-                }}
-                    className={`ui selection dropdown ${open && 'visible active'}`}
-                >
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open && 'visible active'}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
                     <div className={`menu ${open && 'visible transition'}`}>
