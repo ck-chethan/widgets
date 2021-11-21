9df5fd61ab6039487a2ec1cd3413ed8c41b2fffc
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-21 09:47:27 +0530

    Adding Basic Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Dropdown from './components/Dropdown'
+import Header from './components/Header';
+import Route from './components/Route';
 import Search from './components/Search'
 import Translate from './components/Translate';
 
@@ -34,41 +36,56 @@ const options = [
     }
 ];
 
-const showAccordian = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} />
-    }
-}
+// const showAccordian = () => {
+//     if (window.location.pathname === '/') {
+//         return <Accordion items={items} />
+//     }
+// }
 
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />
-    }
-}
+// const showList = () => {
+//     if (window.location.pathname === '/list') {
+//         return <Search />
+//     }
+// }
 
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown />
-    }
-}
+// const showDropdown = () => {
+//     if (window.location.pathname === '/dropdown') {
+//         return <Dropdown />
+//     }
+// }
 
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate />
-    }
-}
+// const showTranslate = () => {
+//     if (window.location.pathname === '/translate') {
+//         return <Translate />
+//     }
+// }
 
 // const showComponents = (route, compnent) => {
 //     return (window.location.pathname === route ? compnent : null);
 // }
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div className="">
-            {showAccordian()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     )
 }
